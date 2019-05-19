48556e3323e040672d96260e0c0c12eb1963bf57
Nathan Hussey <nathanhussey@Nathans-MacBook-Pro.local>
2019-05-18 20:16:51 -0400

    updated clock design

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-let displaySeconds = document.querySelector(".text-center")
+let displayTime = document.querySelector("#clock")
 
-let seconds = new Date();
+let clockIsUpdating;
+
+function currentClockTime() {
+	let currentTime = new Date();
+	let	currentSecond = currentTime.getSeconds();
+	let	currentMinute = currentTime.getMinutes();
+	let	currentHour = currentTime.getHours();
+	if (currentMinute < 10){
+		currentMinute = "0" + currentMinute;
+	}
+	if (currentSecond < 10){
+		currentSecond = "0" + currentSecond;
+	}
+
+	printTime =` ${currentHour} : ${currentMinute} : ${currentSecond}`;
+	displayTime.innerText = printTime
+
+};
+	
+setInterval(currentClockTime, 1000);
 
 
-displaySeconds.innerText = seconds.getSeconds()
