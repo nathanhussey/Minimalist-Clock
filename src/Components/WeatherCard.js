1387a52d6f9d498e30ebc32e387d28bdaf52db92
Nathan Hussey <nathanhussey@Nathans-MacBook-Pro.local>
2019-06-28 22:49:45 -0400

    add days of the week

--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
-const WeatherCard = ({displayWeather,sunset1}) => {
+
+const WeatherCard = ({descrip,icon,day}) => {
+	let date = new Date(day);
+	let dayOfWeek =date.getDay();
+	let theDay='';
+	switch (dayOfWeek) {
+  case 0:
+    theDay = "Sunday";
+    break;
+  case 1:
+    theDay = "Monday";
+    break;
+  case 2:
+     theDay = "Tuesday";
+    break;
+  case 3:
+    theDay = "Wednesday";
+    break;
+  case 4:
+    theDay = "Thursday";
+    break;
+  case 5:
+    theDay = "Friday";
+    break;
+  case 6:
+    theDay = "Saturday";
+    break;
+  default: ;
+  
+
+	}
+
 	return (
-		<div>
-			<h1>{displayWeather}</h1>
-			<h1>{sunset1}</h1>
+		<div className = "bg-light-blue br3 grow ma3 pa3 tc dib">
+			<h1>{theDay}</h1>
+			<img src={`https://www.weatherbit.io/static/img/icons/${icon}.png`} alt = "weatherIcon"/>
+
+			<h3>{descrip}</h3>
 
 
 
@@ -13,6 +46,6 @@ const WeatherCard = ({displayWeather,sunset1}) => {
 		</div>
 	);
 
-}
+};
 
 export default WeatherCard;
