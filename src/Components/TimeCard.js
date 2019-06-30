82a8788fde3bd43ad46cf9e75e4fa91615e6d107
Nathan Hussey <nathanhussey@Nathans-MacBook-Pro.local>
2019-06-29 22:59:22 -0400

    add react clock comp

---
+++ src/Components/TimeCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TimeCard = ({theTime}) => {
+
+	return(
+		<div className = "grow ma3 tc dib">
+			<h1>{theTime}</h1>
+
+		</div>
+
+
+
+	)
+
+
+
+}
+export default TimeCard;
